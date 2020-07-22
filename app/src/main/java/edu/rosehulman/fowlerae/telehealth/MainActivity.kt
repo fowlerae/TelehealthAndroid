57d99969c07ce161343b6e49e1f8d02dc6f56b9e
Ashley <fowlerae@rose-hulman.edu>
2020-07-22 19:48:33 -0400

    trying to fix symptom fragment

--- app/src/main/java/edu/rosehulman/fowlerae/telehealth/MainActivity.kt
+++ app/src/main/java/edu/rosehulman/fowlerae/telehealth/MainActivity.kt
@@ -62,6 +62,7 @@ class MainActivity : AppCompatActivity() {
 //            } else {
 //                launchLoginUI()
 //            }
+            auth.signOut()
             launchLoginUI()
         }
     }
