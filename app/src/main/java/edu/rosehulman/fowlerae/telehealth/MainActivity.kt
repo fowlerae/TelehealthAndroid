840f65ff9d0496e656602c73da47e68d0715877b
Ashley <fowlerae@rose-hulman.edu>
2020-07-23 13:25:21 -0400

    trying to fix symptom fragment

--- app/src/main/java/edu/rosehulman/fowlerae/telehealth/MainActivity.kt
+++ app/src/main/java/edu/rosehulman/fowlerae/telehealth/MainActivity.kt
@@ -53,16 +53,15 @@ class MainActivity : AppCompatActivity() {
         authListener = FirebaseAuth.AuthStateListener { auth ->
             val user = auth.currentUser
             Log.d(Constants.TAG, "In auth listener, User: $user")
-//            if (user != null) {
-//                Log.d(Constants.TAG, "UID: ${user.uid}")
-//                Log.d(Constants.TAG, "Name: ${user.displayName}")
-//                Log.d(Constants.TAG, "Email: ${user.email}")
-//                Log.d(Constants.TAG, "Photo: ${user.photoUrl}")
-//                Log.d(Constants.TAG, "Phone: ${user.phoneNumber}")
-//            } else {
-//                launchLoginUI()
-//            }
-            launchLoginUI()
+            if (user != null) {
+                Log.d(Constants.TAG, "UID: ${user.uid}")
+                Log.d(Constants.TAG, "Name: ${user.displayName}")
+                Log.d(Constants.TAG, "Email: ${user.email}")
+                Log.d(Constants.TAG, "Photo: ${user.photoUrl}")
+                Log.d(Constants.TAG, "Phone: ${user.phoneNumber}")
+            } else {
+                launchLoginUI()
+            }
         }
     }
 
